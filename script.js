c764e29fae8e12a98f367783292846606d49e95f
JohnnyUtah666 <porksloppp@gmail.com>
2024-05-03 13:55:42 -0700

    fill out book constructor; write function to add books to library

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const myLibrary = [];
 
-function Book() {
-
+function Book(title, author, pageCount) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
 }
 
-function addBookToLibrary() {
+const jest = new Book("Infinite Jest", "David Foster Wallace", 1056);
 
+function addBookToLibrary(Book) {
+    myLibrary.push(Book);
 }
+
+addBookToLibrary(jest);
+
+console.log(myLibrary);
+
 
 const newBook = document.querySelector('.newBook');
 newBook.addEventListener("click", (event) => {
