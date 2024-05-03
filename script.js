c13f687cce7a6f908da449d0aa905876e1dd6e6e
JohnnyUtah666 <porksloppp@gmail.com>
2024-05-03 13:16:42 -0700

    lay framework for functions that will be written; target button and make it responsive for later use

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
+
+const newBook = document.querySelector('.newBook');
+newBook.addEventListener("click", (event) => {
+    alert("You have clicked this button");
+});
+
